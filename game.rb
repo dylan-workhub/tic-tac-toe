2cd42e1b6854b172f440b5ffd6d072a14bcdad43
Dylan <dylandixon@hotmail.com>
2021-05-13 03:05:16 -0230

    turn board array into hash, make changes to logic to print board

--- game.rb
+++ game.rb
@@ -1,28 +1,34 @@
-#board will be a class, along with X's and O's
+# board will be a class, along with X's and O's
 class GameBoard
+  attr_accessor :board_hash
+
   def initialize
-    @board_array = [
-      ["1", "|", "2", "|", "3"],
-      ["---------"],
-      ["4", "|", "5", "|", "6"],
-      ["---------"],
-      ["7", "|", "8", "|", "9"]
-    ]
-    self.print_game_board
+    @board_hash = {
+      1 => { symbol: 1, played: false }, 2 => { symbol: 2, played: false },
+      3 => { symbol: 3, played: false }, 4 => { symbol: 4, played: false },
+      5 => { symbol: 5, played: false }, 6 => { symbol: 6, played: false },
+      7 => { symbol: 7, played: false }, 8 => { symbol: 8, played: false },
+      9 => { symbol: 9, played: false }
+    }
+    print_game_board
   end
 
-  protected
   def print_game_board
-    @board_array.each do |row|
-      puts row.join(' ')
-    end
+    puts "#{@board_hash[1][:symbol]} | #{@board_hash[2][:symbol]} | #{@board_hash[3][:symbol]}"
+    puts '---------'
+    puts "#{@board_hash[4][:symbol]} | #{@board_hash[5][:symbol]} | #{@board_hash[6][:symbol]}"
+    puts '---------'
+    puts "#{@board_hash[7][:symbol]} | #{@board_hash[8][:symbol]} | #{@board_hash[9][:symbol]}"
   end
 end
 
+# class for all players
 class Player
   def initialize(symbol)
     @symbol = symbol
   end
+
+  def place_symbol(location) end
 end
 
 newgame = GameBoard.new
