632f3ffc275006ff1e9027673874f68c693cf2a3
Dylan <dylandixon@hotmail.com>
2021-05-12 22:22:18 -0230

    Add logic for initializing gameboard, add Player class.

--- game.rb
+++ game.rb
@@ -1,16 +1,28 @@
 #board will be a class, along with X's and O's
-
 class GameBoard
-  @board_array = [["1", "|", "2", "|", "3"], ["---------"], ["4", "|", "5", "|", "6"],["---------"], ["7", "|", "8", "|", "9"]]
-
   def initialize
-
+    @board_array = [
+      ["1", "|", "2", "|", "3"],
+      ["---------"],
+      ["4", "|", "5", "|", "6"],
+      ["---------"],
+      ["7", "|", "8", "|", "9"]
+    ]
+    self.print_game_board
   end
 
-  private
+  protected
   def print_game_board
     @board_array.each do |row|
       puts row.join(' ')
     end
   end
 end
+
+class Player
+  def initialize(symbol)
+    @symbol = symbol
+  end
+end
+
+newgame = GameBoard.new
